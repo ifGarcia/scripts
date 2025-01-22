75669546c871e0b98730c8ed869dcd00a65cfcbb
Bruno Vinicius Garcia <44682187+ifGarcia@users.noreply.github.com>
2025-01-22 15:42:09 -0300

    Update script.py

--- script.py
+++ script.py
@@ -1,4 +1,12 @@
 import os
-print('Environment Variables:')
-for key, value in os.environ.items():
-  print(f'{key}: {value}')
+
+# Pegando variáveis de ambiente
+my_var = os.getenv('MY_VAR')
+
+# Função para imprimir variáveis de ambiente
+def print_env_vars():
+    print('Environment Variables:')
+    print(f'MY_VAR: {my_var}')
+
+# Chamando a função
+print_env_vars()
